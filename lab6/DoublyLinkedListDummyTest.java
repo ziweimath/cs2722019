1bcb0478245fd422dce4d9f5264fa62bcf7608a2
ziweimath <ziweinmsu@gmail.com>
2019-02-27 18:56:25 -0700

    Update DoublyLinkedListDummyTest.java
    Wed

--- lab6/DoublyLinkedListDummyTest.java
+++ lab6/DoublyLinkedListDummyTest.java
@@ -4,6 +4,8 @@ public class DoublyLinkedListDummyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Test how DIntNode constructor works
+		DoublyLinkedListDummy newDIntNodeWD = new DoublyLinkedListDummy();
+		System.out.println("The dummy head points to "+newDIntNodeWD.getTail().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getHead().getData());
 		DIntNode newDIntNode = new DIntNode(0,null,null);
 		System.out.println("The data in thos Node is "+newDIntNode.getData()+". The prev node is "+newDIntNode.getPrev()+". The next node is "+newDIntNode.getNext());
 		DIntNode newDIntNode1 = new DIntNode(1,null,null);
@@ -20,10 +22,31 @@ public class DoublyLinkedListDummyTest {
 		System.out.println(newDIntNode2);
 		System.out.println(newDIntNode3);
 		
-		
-		
-		
-		
+		// #4 add method
+		newDIntNodeWD.addEnd(3);
+		System.out.println("The dummy head points to "+newDIntNodeWD.getHead().getNext().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getTail().getPrev().getData());
+		newDIntNodeWD.addEnd(4);
+		System.out.println("The dummy head points to "+newDIntNodeWD.getHead().getNext().getNext().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getTail().getPrev().getData());
+		// #5 removeFromHead method
+		newDIntNodeWD.removeFromHead();
+		System.out.println("The dummy head points to "+newDIntNodeWD.getHead().getNext().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getTail().getPrev().getData());
+		newDIntNodeWD.addEnd(3);
+		newDIntNodeWD.removeFromHead();
+		System.out.println("The dummy head points to "+newDIntNodeWD.getHead().getNext().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getTail().getPrev().getData());
+		//System.out.println("The dummy head points to "+newDIntNodeWD.getTail().getData() + "\nThe dummy tail points to " + newDIntNodeWD.getHead().getData());
+		newDIntNodeWD.addEnd(4);
+		newDIntNodeWD.addEnd(3);
+		newDIntNodeWD.addEnd(1);
+		newDIntNodeWD.addEnd(3);
+		newDIntNodeWD.addEnd(1);
+		//newDIntNodeWD.removeFromHead();
+		//System.out.println("The values in this doublely linked list are "+newDIntNodeWD);
+		//newDIntNodeWD.removeFromHead();
+		//System.out.println("The values in this doublely linked list are "+newDIntNodeWD);
+		//newDIntNodeWD.removeFromHead();
+		System.out.println("The values in this doublely linked list are "+newDIntNodeWD);
+		// int num = newDIntNodeWD.countOccurence(3);
+		System.out.println("The number of value "+ 3 +"appering in this doublely linked is ");
 		
 		
 		
